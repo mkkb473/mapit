521bb8a1de79b1fb79178468a27f36104eda7e89
mkkb473 <yaoyangliu@outlook.com>
2018-08-22 18:45:58 +0800

    second commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/naturali/mapit/merger"
 )
 
 func init() {
-	merger.SamplingSize = 1000000
+	merger.SamplingSize = 4000000
 }
 
 func main() {
@@ -16,4 +18,13 @@ func main() {
 		arraySize *= 4
 	}
 
+	retArrayP := &kvsArray[0]
+	for i := 1; i < len(kvsArray); i++ {
+		retArrayP = merger.MergeTwoArries(retArrayP, &kvsArray[i])
+	}
+
+	for _, iter := range *retArrayP {
+		fmt.Println(iter)
+	}
+	fmt.Println(len(*retArrayP))
 }
--- merger/merger.go
+++ merger/merger.go
@@ -1,6 +1,7 @@
 package merger
 
 import (
+	"bytes"
 	"encoding/binary"
 	"math/rand"
 	"sort"
@@ -44,15 +45,54 @@ func MakeSortedRandKVArray(keyNum int) []KV {
 	sort.Ints(keys)
 	for _, value := range keys {
 		bs := make([]byte, 4)
-		binary.LittleEndian.PutUint32(bs, uint32(value))
+		binary.BigEndian.PutUint32(bs, uint32(value))
 		rand.Seed(time.Now().UnixNano())
+		data := rand.Perm(20)[:rand.Intn(4)+1]
+		sort.Ints(data)
 		kvArr = append(kvArr, KV{
 			Key: bs,
 			Value: Iterator{
-				Data: rand.Perm(20)[:rand.Intn(4)+1],
+				Data: data,
 				I:    0,
 			},
 		})
 	}
 	return kvArr
+}
+
+func MergeTwoArries(A, B *[]KV) *[]KV {
+	var C []KV
+	// A should always be shorter than B
+	if len(*A) > len(*B) {
+		A, B = B, A
+	}
+
+	var headA, headB int
+	for headA != len(*A) && headB != len(*B) {
+		bytesCompare := bytes.Compare((*A)[headA].Key, (*B)[headB].Key)
+		if bytesCompare < 0 {
+			C = append(C, (*A)[headA])
+			headA++
+		} else if bytesCompare == 0 {
+			C = append(C, (*A)[headA])
+			headA++
+			headB++
+		} else {
+			C = append(C, (*B)[headB])
+			headB++
+		}
+	}
+	if headA != len(*A) {
+		for headA < len(*A) {
+			C = append(C, (*A)[headA])
+			headA++
+		}
+	}
+	if headB != len(*B) {
+		for headB < len(*B) {
+			C = append(C, (*B)[headB])
+			headB++
+		}
+	}
+	return &C
 }
